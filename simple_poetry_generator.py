2c16153cef5c6d4b1cf630dbe9a6cb9ca4ce3abe
husen <husentod10@gmail.com>
2018-01-31 22:44:55 +0700

    add answers

--- simple_poetry_generator.py
+++ simple_poetry_generator.py
@@ -1,6 +1,10 @@
 import nltk
 import random
+import sys
 from collections import Counter
+
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 
 file = open('_poetry_khairil_anwar.txt')
@@ -9,15 +13,25 @@ poem = file.read()
 token = nltk.word_tokenize(poem)
 
 # NLTK shortcuts :)
-bigrams = nltk.bigrams(token)
-cfd = nltk.ConditionalFreqDist(bigrams)
+bigrams = nltk.ngrams(token,1)
+cfd = Counter(nltk.ConditionalFreqDist(bigrams))
+
+
+
+# for c in cfd:
+# 	print Counter(cfd[c])
+
+print "INI PALING ", cfd.most_common(10)
+
+# print Counter(cfd)
+
 
 # pick a random word
-word = random.choice(token)
+# word = random.choice(token)
 
-for i in range(15):
-    print word,
-    if word in cfd:
-        word = random.choice(list(cfd[word].keys()))
-    else:
-        break
+# for i in range(15):
+#     print word,
+#     if word in cfd:
+#         word = random.choice(list(cfd[word].keys()))
+#     else:
+#         break
